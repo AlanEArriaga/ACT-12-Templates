2f1d96e9dc671a00cdf4c8e8c8ff98d49ae51cc9
Alan Echeveste <alan.echeveste2963@alumnos.udg.mx>
2020-10-26 17:19:57 -0600

    Funciona las eliminaciones

--- arreglo.h
+++ arreglo.h
@@ -18,6 +18,11 @@ public:
     void insertarFinal(const T& v);
     void insertarInicio(const T& v);
     void insertar(const T& v, size_t p);
+
+    void eliminarFinal();
+    void eliminarInicio();
+    void elminar(size_t p);
+
     size_t size();
     int operator[](size_t p)
     {
@@ -78,6 +83,40 @@ void Arreglo<T>::insertar(const T& v, size_t p){
     arreglo[p] = v;
     cont++;
 }
+
+template <class T>
+void Arreglo<T>::eliminarFinal(){
+    if(cont == 0){
+        cout << "Arreglo esta vacion" << endl;
+        return;
+    }
+    cont--;
+}
+
+template <class T>
+void Arreglo<T>::eliminarInicio(){
+    if(cont == 0){
+        cout << "Arreglo esta vacion" << endl;
+        return;
+    }
+    for(size_t i = 0; i < cont-1; i++){
+        arreglo[i] = arreglo[i+1];
+    }
+    cont--;
+}
+
+template <class T>
+void Arreglo<T>::elminar(size_t p){
+    if(cont == 0){
+        cout << "Arreglo esta vacion" << endl;
+        return;
+    }
+    for(size_t i = p ; i < cont-1; i++){
+        arreglo[i] = arreglo [i+1];
+    }
+    cont--;
+}
+
 
 template <class T>
 size_t Arreglo<T>::size(){
--- main.cpp
+++ main.cpp
@@ -11,10 +11,13 @@ int main()
         arreglo.insertarFinal(i);
     }
     arreglo.insertar(100, 2);
-    for(size_t i = 0; i < 10; i++){
+    arreglo.eliminarInicio();
+    arreglo.eliminarFinal();
+    arreglo.elminar(4);
+    for(size_t i = 0; i < arreglo.size(); i++){
         cout <<arreglo[i]<< " ";
     }
-    
+
     cout << endl;
     return 0;
 }
